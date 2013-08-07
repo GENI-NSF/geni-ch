2a867bc2d23d4fa966f8a135ce0674c20bf9c8ef
Marshall Brinn <mbrinn@bbn.com>
2013-08-07 09:37:25 -0400

    Refactoring of ABAC guard stuff

--- chapi/plugins/sarm/SAv1Guard.py
+++ chapi/plugins/sarm/SAv1Guard.py
@@ -4,14 +4,19 @@ from  sqlalchemy import *
 from chapi.GuardBase import GuardBase
 from chapi.Exceptions import *
 import sfa.trust.certificate;
+import types
 from ABAC import *
 from tools.ABACManager import ABACManager
 
 # Pre-processor for method invocations
-class SAv1InvocationGuard:
+class SAv1InvocationGuard(object):
 
     def __init__(self):
-        self.db_engine = pm.getService('chdbengine')
+        self._db_engine = pm.getService('chdbengine')
+        self._client_cert = None
+
+    def setClientCert(self, client_cert):
+        self._client_cert = client_cert
 
     # Raise an AUTHENTICATION_ERROR if there is something wrong about the 
     # certs and credentials passed to the call
@@ -27,7 +32,7 @@ class SAv1InvocationGuard:
 
     # Raise an AUTHORIZATION_ERROR if there is something wrong about the 
     # certs and credentials and options/argumentspassed to the call
-    def authorize_call(self, client_cert, credentials, options, args):
+    def authorize_call(self, client_cert, credentials, args):
         # Method-specific logic
         pass
 
@@ -35,7 +40,7 @@ class SAv1InvocationGuard:
     def validate(self, client_cert, credentials, options, args):
         self.authenticate_call(client_cert, credentials)
         self.validate_arguments(options, args)
-        self.authorize_call(client_cert, credentials, options, args)
+        self.authorize_call(client_cert, credentials, args)
 
 # Some helper methods
 
@@ -63,7 +68,7 @@ def lookup_project_names_for_user(user_urn):
     db = pm.getService('chdbengine')
     session = db.getSession()
 
-    q = session.query(db.MEMBER_ATTRIBUTE_TABLE, db.PROJECT_TABLE, db.PROJECT_MEMBER_TABLE)
+    q = session.query(db.PROJECT_TABLE, db.MEMBER_ATTRIBUTE_TABLE, db.PROJECT_MEMBER_TABLE)
     q = q.filter(db.PROJECT_TABLE.c.expired == 'f')
     q = q.filter(db.MEMBER_ATTRIBUTE_TABLE.c.name == 'urn')
     q = q.filter(db.PROJECT_TABLE.c.project_id == db.PROJECT_MEMBER_TABLE.c.project_id)
@@ -91,126 +96,123 @@ def lookup_operator_privilege(user_urn):
     rows = q.all()
     return len(rows) > 0
 
-# Post-processor for invocations
-# For individual rows that were computed, are they permitted to be
-# seen by caller?
-class SAv1RowGuard:
+class ABACAsserter(object): pass
 
-    def __init__(self):
-        self.db_engine = pm.getService('chdbengine')
-
-    def permit(self, client_cert, credentials, urn, urn_results):
-        return True
-
-class LookupSlicesInvocationGuard(SAv1InvocationGuard): 
-    def authorize_call(self, client_cert, credentials, options, args):
-        pass
-        
-class LookupSliceMembersInvocationGuard(SAv1InvocationGuard): 
-    def authorize_call(self, client_cert, credentials, options, args):
+class OperatorAsserter(ABACAsserter):
+    def generate_assertions(self, abac_manager, client_cert, credentials, args):
         user_urn = extract_user_urn(client_cert)
-        slice_urn = args['slice_urn']
-        slice_project_name = lookup_project_name_for_slice(slice_urn)
-#        print "PROJECT_NAME = " + slice_project_name
-        config = pm.getService('config')
-        key_file = config.get("chapiv1rpc.ch_key")
-        cert_file = config.get("chapiv1rpc.ch_cert")
-        # Bind entities : C = clien_cert, SA = sa_cert, sa_key
-        certs = {'C' : client_cert}
-        abac_manager = ABACManager("SA", cert_file, key_file, certs)
-
-        # Gather assertions:
-        # MA.is_member_Pi<-C for each project that C is a member of
-        #  MA.is_operator<-C [If C is an operator]
-        user_project_names = lookup_project_names_for_user(user_urn)
-        for user_project_name in user_project_names:
-            assertion = "SA.is_member_%s<-C" % user_project_name
-            abac_manager.register_assertion(str(assertion))
         is_operator = lookup_operator_privilege(user_urn)
         if is_operator:
             abac_manager.register_assertion("SA.is_operator<-C")
 
-        # Try to prove any of:
-        #   MA.is_operator<- C
-        #   MA.is_member_P <- C [For the project of the slice URN]
-        queries = [
-            {'role' : 'is_operator', 'target' : 'C'},
-            {'role' : str('is_member_%s' % slice_project_name), 'target' : 'C'}
-            ]
-        for q in queries:
-            q_role = q['role']
-            q_target = q['target']
-            ok, proof =  abac_manager.query(q_target, q_role)
-            if ok:
-                print "Proof " + "\n".join(abac_manager.pretty_print_proof(proof))
-                return
-        raise CHAPIv1AuthorizationError("Caller %s is not allowed to access slice_membership for slice %s" % (user_urn, slice_urn))
+class ProjectMemberAsserterByURN(ABACAsserter):
+    def generate_assertions(self, abac_manager, client_cert, credentials, args):
+        user_urn = args['urn']
+        user_project_names = lookup_project_names_for_user(user_urn)
+        for user_project_name in user_project_names:
+            assertion = "SA.is_member_%s<-C" % str(user_project_name)
+            abac_manager.register_assertion(assertion)
 
-class LookupSlicesRowGuard(SAv1RowGuard): 
-    def permit(self, client_cert, credentials, urn, urn_results):
-        urn_project_name = lookup_project_name_for_slice(urn)
-#        print "PROJECT_NAME = " + urn_project_name
-        config = pm.getService('config')
-        key_file = config.get("chapiv1rpc.ch_key")
-        cert_file = config.get("chapiv1rpc.ch_cert")
+class ProjectMemberAsserterByCert(ProjectMemberAsserterByURN):
+    def generate_assertions(self, abac_manager, client_cert, credentials, args):
+        args['urn'] = extract_user_urn(client_cert)
+        super(self.__class__, self).generate_assertions(abac_manager, \
+                                                            client_cert, credentials, args)
+
+class QueryGenerator(object): pass
+
+# Is client (C) a member of the project associated with the given slice URN?
+class QueryProjectMember(QueryGenerator):
+    def generate_query(self, client_cert, credentials, args):
+        slice_urn = args['urn']
+        urn_project_name = lookup_project_name_for_slice(slice_urn)
+        return "C", "is_member_%s" %  str(urn_project_name)
+
+# Is client (C) a member of the project associated
+class QueryProjectMemberBySliceURN(QueryGenerator):
+    def generate_query(self, client_cert, credentials, args):
+        slice_urn = args['slice_urn']
+        urn_project_name = lookup_project_name_for_slice(slice_urn)
+        return "C", "is_member_%s" %  str(urn_project_name)
+
+class SAv1ABACGuard(SAv1InvocationGuard):
+
+    def __init__(self, asserters, queries):
+        self._asserters = asserters
+        self._queries = queries
+        self.config = pm.getService('config')
+        self.key_file = self.config.get("chapiv1rpc.ch_key")
+        self.cert_file = self.config.get("chapiv1rpc.ch_cert")
 #        print "KEY = " + key_file
 #        print "CERT = " + cert_file
-#        print "CLIENT_CERT = " + str(client_cert)
-        user_urn = extract_user_urn(client_cert)
-        if user_urn == None:
-            raise CHAPIv1AuthorizationError("Certificate has no subjectAltName publicid URN")
 
+    def compute(self, client_cert, credentials, args):
         # Bind entities : C = client_cert, SA = sa_cert, sa_key
         certs = {'C' : client_cert}
-        abac_manager = ABACManager("SA", cert_file, key_file, certs)
+        abac_manager = ABACManager('SA', self.cert_file, self.key_file, certs)
 
-        # Gather assertions:
-        #   MA.is_operator<- C [If C is an operator]
-        #   MA.is_member_Pi<-C [For each project that C is a member of]
-        user_project_names = lookup_project_names_for_user(user_urn)
-        for user_project_name in user_project_names:
-            assertion = "SA.is_member_%s<-C" % user_project_name
-            abac_manager.register_assertion(str(assertion))
-        is_operator = lookup_operator_privilege(user_urn)
-        if is_operator:
-            abac_manager.register_assertion("SA.is_operator<-C")
+        # Gather all assertions about context
+        for asserter in self._asserters:
+            asserter.generate_assertions(abac_manager, client_cert, credentials, args)
 
-        # Try to prove any of:
-        #   MA.is_operator<- C
-        #   MA.is_member_P <- C [For the project of the slice URN]
-        queries = [
-            {'role' : 'is_operator', 'target' : 'C'},
-            {'role' : str('is_member_%s' % urn_project_name), 'target' : 'C'}
-            ]
-        for q in queries:
-            q_role = q['role']
-            q_target = q['target']
+        # Compute queries from ABAC: If any pass, we permit.
+        for query in self._queries:
+            if isinstance(query, QueryGenerator):
+                q_target, q_role = query.generate_query(client_cert, credentials, args)
+            else:
+                q_target = query[0]
+                q_role = query[1]
+
             ok, proof = abac_manager.query(q_target, q_role)
             if ok:
                 print "Proof " + "\n".join(abac_manager.pretty_print_proof(proof))
                 return True
         return False
 
+# For testing whether a method invocation is permitted
+class SAv1ABACInvocationGuard(SAv1ABACGuard):
+
+    def __init__(self, asserters, queries):
+        super(self.__class__, self).__init__(asserters, queries)
+
+    def authorize_call(self, client_cert, credentials, args):
+        return self.compute(client_cert, credentials, args = args)
+
+# For testing whether a given data row is permitted to be exposed to caller
+class SAv1ABACRowGuard(SAv1ABACGuard):
+    def __init__(self, asserters, queries): 
+        super(self.__class__, self).__init__(asserters, queries)
+
+    def permit(self, client_cert, credentials, urn):
+        return self.compute(client_cert, credentials, args = {'urn' : urn})
+
 INVOCATION_GUARDS_FOR_METHOD = \
-    { 'lookup_slices' : LookupSlicesInvocationGuard(),
-      'lookup_slice_members' : LookupSliceMembersInvocationGuard()
-      }
+    { 
+    'lookup_slices' : 
+    SAv1ABACInvocationGuard(asserters = [], queries = []),
+    'lookup_slice_members' : 
+    SAv1ABACInvocationGuard(asserters= [OperatorAsserter(), ProjectMemberAsserterByCert()],
+                                queries = [["C", "is_operator"], QueryProjectMemberBySliceURN()])
+    }
 
 ROW_GUARDS_FOR_METHOD = \
-    { 'lookup_slices' : LookupSlicesRowGuard()
-      }
+    { 
+    'lookup_slices' : 
+    SAv1ABACRowGuard(asserters = [OperatorAsserter(), ProjectMemberAsserterByCert()],
+                     queries = [["C", "is_operator"], QueryProjectMember()]) 
+    }
 
 class SAv1Guard(GuardBase):
     
     def __init__(self):
-        super(SAv1Guard, self).__init__()
+        super(self.__class__, self).__init__()
 
     def validate_call(self, client_cert, method, credentials, options, args):
         print "SAv1Guard.validate_call : " + method + " " + str(args) + " " + str(options)
         if INVOCATION_GUARDS_FOR_METHOD.has_key(method):
             invocation_guard = INVOCATION_GUARDS_FOR_METHOD[method]
-            return invocation_guard.validate(client_cert, credentials, \
-                                                 options, args)
+            invocation_guard.setClientCert(client_cert)
+            return invocation_guard.validate(client_cert, credentials, options, args)
 
     def protect_results(self, client_cert, method, credentials, results):
         print "SAv1Guard.protect_results : " + method + " " + str(results)
@@ -218,8 +220,9 @@ class SAv1Guard(GuardBase):
         if ROW_GUARDS_FOR_METHOD.has_key(method):
             protected_results = {}
             row_guard = ROW_GUARDS_FOR_METHOD[method]
+            row_guard.setClientCert(client_cert)
             for urn in results.keys():
                 urn_result = results[urn]
-                if row_guard.permit(client_cert, credentials, urn, urn_result):
+                if row_guard.permit(client_cert, credentials, urn):
                     protected_results[urn] = urn_result
         return protected_results
