56d9678e1f37d246419216e91a2f5f9febc50b05
Marshall Brinn <mbrinn@bbn.com>
2013-08-11 13:01:37 -0400

    Add some comments to SAv1Guard

--- chapi/plugins/sarm/SAv1Guard.py
+++ chapi/plugins/sarm/SAv1Guard.py
@@ -1,9 +1,14 @@
 from ABACGuard import *
 
+# Specific guard for GPO SA
+# Provide a set of invocation checks and row checks per method
 class SAv1Guard(ABACGuardBase):
 
     INVOCATION_CHECKS_FOR_METHOD = \
         { 
+        # lookup_slice_members can be called by anyone who is either
+        #   - a member of the project to which the slice belongs
+        #   - an operator
         'lookup_slice_members' : 
         ABACInvocationCheck(asserters= [
                 OperatorAsserter(), 
@@ -14,6 +19,8 @@ class SAv1Guard(ABACGuardBase):
 
     ROW_CHECKS_FOR_METHOD = \
         { 
+        # Rows returned from lookup_slices must belong to a project that the caller belongs to
+        # Unless the requester is an operator, in which case all rows are okay to return
         'lookup_slices' : 
         ABACRowCheck(asserters = [
                 OperatorAsserter(), 
