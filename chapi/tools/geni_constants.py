35d0ec79bc6f1f76969fa1179beb92c48766b4ba
Marshall Brinn <mbrinn@bbn.com>
2013-09-02 11:50:52 -0400

    File defining some geni constants (roles, context_type)

---
+++ chapi/tools/geni_constants.py
@@ -0,0 +1,40 @@
+#----------------------------------------------------------------------
+# Copyright (c) 2011-2013 Raytheon BBN Technologies
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and/or hardware specification (the "Work") to
+# deal in the Work without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Work, and to permit persons to whom the Work
+# is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Work.
+#
+# THE WORK IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE WORK OR THE USE OR OTHER DEALINGS
+# IN THE WORK.
+#----------------------------------------------------------------------
+
+# Sets of constants for defining relationships and roles in GENI 
+# Registry and Authorities
+
+# Context Types
+PROJECT_CONTEXT = 1
+SLICE_CONTEXT = 2
+RESOURCE_CONTEXT = 3
+SERVICE_CONTEXT = 4
+MEMBER_CONTEXT = 5
+
+# Attribute (role) Types
+LEAD_ATTRIBUTE = 1
+ADMIN_ATTRIBUTE = 2
+MEMBER_ATTRIBUTE = 3
+AUDITOR_ATTRIBUTE = 4
+OPERATOR_ATTRIBUTE = 5
+
