66d8051a299bda8092b220c18f6e92b8ef25c12f
Marshall Brinn <mbrinn@bbn.com>
2013-09-03 14:29:54 -0400

    Change query to request

--- proto-ch/lib/php/chapi.php
+++ proto-ch/lib/php/chapi.php
@@ -61,7 +61,7 @@ class XMLRPCClient
     curl_setopt($ch, CURLOPT_URL, $this->url);
     curl_setopt($ch, CURLOPT_POST, true);
     curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $query);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $request);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // enable this
