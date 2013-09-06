a59c3d4493d1b77aaf97fe4b096ba77c8b74b782
David Montana <dmontana@bbn.com>
2013-09-06 15:43:00 -0400

    more changes to support install

--- chapi/plugins/chrm/plugin.py
+++ chapi/plugins/chrm/plugin.py
@@ -36,7 +36,7 @@ def setup():
 
     # set up config keys
     config = pm.getService('config')
-    config.install("chrm.db_url", None, "database URL")
+    config.install('chrm.db_url', 'postgresql://portal:portal@localhost/portal', 'database URL')
 
     config.install("chrm.authority", "ch-mb.gpolab.bbn.com", \
                        "name of CH/SA/MA authority")
