d91095de8008b0dfa0aaa86e7c6956db53499812
Tom Mitchell <tmitchell@bbn.com>
2013-11-08 11:15:54 -0500

    Read parameters from a config file.
    Have Parameters load internal defaults and then read a config file to
    override those defaults. Config file format is INI so that it can be
    parsed in a variety of tools.

--- chapi/plugins/chapiv1rpc/chapi/Parameters.py
+++ chapi/plugins/chapiv1rpc/chapi/Parameters.py
@@ -23,12 +23,20 @@
 
 import amsoil.core.pluginmanager as pm
 import os.path
+import ConfigParser
+from tools.chapi_log import *
+
+CONFIG_FILE = '/etc/geni-chapi/chapi.ini'
 
 GCF_ROOT = "/usr/share/geni-ch/portal/gcf.d"
 
 VERSION_NUMBER = '1.0'
 
-parameters = [
+NAME_KEY = 'name'
+VALUE_KEY = 'val'
+DESC_KEY = 'desc'
+
+default_parameters = [
     {
         'name': 'chapiv1rpc.ch_cert_root', 
         'val': os.path.join(GCF_ROOT, 'trusted_roots'),
@@ -107,8 +115,62 @@ parameters = [
 ]
 
 
+def get_typed_value(parser, section, option, value_type):
+    """Get a typed value from a ConfigParser.
+
+    Use the right ConfigParser accessor to get the correct type
+    from the ConfigParser. If type is unknown, return None.
+    """
+    value = None
+    if value_type is str:
+        value = parser.get(section, option)
+    elif value_type is int:
+        value = parser.getint(section, option)
+    elif value_type is bool:
+        value = parser.getboolean(section, option)
+    else:
+        msg = 'Unknown type %s for default parameter %s'
+        chapi_warn('PARAMETERS',
+                   msg % (value_type.__name__, pname))
+    return value
+
+def param_to_secopt(param):
+    """Convert a parameter name to INI section and option.
+    Split on the first dot. If not dot exists, return name
+    as option, and None for section."""
+    sep = '.'
+    sep_loc = param.find(sep)
+    if sep_loc == -1:
+        # no dot in name, skip it
+        section = None
+        option = param
+    else:
+        section = param[0:sep_loc]
+        option = param[sep_loc+1:]
+    return (section, option)
+
 def set_parameters():
     config = pm.getService("config")
-    for param in parameters:
-        config.install(param['name'], param['val'], param['desc'])
-        config.set(param['name'], param['val'])
+    # Set up the defaults
+    for param in default_parameters:
+        config.install(param[NAME_KEY], param[VALUE_KEY], param[DESC_KEY])
+    # Overwrite the defaults with values from the config file
+    parser = ConfigParser.SafeConfigParser()
+    result = parser.read(CONFIG_FILE)
+    if len(result) != 1:
+        # file was not read, warn and return
+        chapi_warn('PARAMETERS',
+                   'Unable to read config file %s' % (CONFIG_FILE))
+    else:
+        for param in default_parameters:
+            pname = param[NAME_KEY]
+            (section, option) = param_to_secopt(pname)
+            if parser.has_option(section, option):
+                value_type = type(param[VALUE_KEY])
+                value = get_typed_value(parser, section, option, value_type)
+                if value:
+                    # If a value was extracted, set it
+                    msg = 'Setting parameter %s to %s from %s'
+                    chapi_debug('PARAMETERS',
+                                msg % (pname, value, CONFIG_FILE))
+                    config.set(pname, value)
