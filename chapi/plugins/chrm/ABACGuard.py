099687a9667a3b7ccf59d7d9e1f4739c4cc11aa5
Aaron Helsinger <ahelsing@bbn.com>
2014-04-17 14:56:58 -0400

    ticket #269: fix spelling of SEARCHING_BY_EMAIL in member authority policy. Fix semantics of the PROJECT_LEAD and PROJECT_ADMIN bindings to fill these in if the _caller_ is one of those roles. This fixes bulk upload from the portal where we a project lead or admin looks up a member by email address and does not share a project with that user.

--- chapi/plugins/chrm/ABACGuard.py
+++ chapi/plugins/chrm/ABACGuard.py
@@ -329,6 +329,8 @@ class SubjectInvocationCheck(InvocationCheck):
 
         authority = pm.getService('config').get("chrm.authority")
 
+#        chapi_info('gen_bindings', "Subject Type: %s; self.bindings: %s; subjects: %s" % (subject_type, self._bindings, subjects))
+
         # Prepare a set of bindings (label => value) for each subject
         bindings_by_subject = {}
         for subject in subjects: 
@@ -383,18 +385,26 @@ class SubjectInvocationCheck(InvocationCheck):
                     for sharer in sharers:
                         bindings_by_subject[sharer][binding] = "SHARES_PROJECT"
             elif binding == "$PROJECT_LEAD":
-                if subject_type == "MEMBER_URN":
-                    leads = has_role_on_some_project(subjects, LEAD_ATTRIBUTE,
-                                                     session)
-                    for lead in leads:
-                        bindings_by_subject[lead][binding] = "PROJECT_LEAD"
+                # Fill in this binding if the _caller_ is a project lead on some project.
+                # Use this EG so a project lead/admin can look up details of people they want to add to a project
+                leads = has_role_on_some_project([caller_urn], LEAD_ATTRIBUTE,
+                                                 session)
+                if caller_urn in leads:
+                    for subject in subjects:
+                        bindings_by_subject[subject][binding] = "PROJECT_LEAD"
             elif binding == "$PROJECT_ADMIN":
-                if subject_type == "MEMBER_URN":
-                    admins = has_role_on_some_project(subjects, ADMIN_ATTRIBUTE,
-                                                      session)
-                    for admin in admins:
-                        bindings_by_subject[admin][binding] = "PROJECT_ADMIN"
+                # Fill in this binding if the _caller_ is a project lead on some project.
+                # Use this EG so a project lead/admin can look up details of people they want to add to a project
+                admins = has_role_on_some_project([caller_urn], ADMIN_ATTRIBUTE,
+                                                  session)
+                if caller_urn in admins:
+                    for subject in subjects:
+                        bindings_by_subject[subject][binding] = "PROJECT_ADMIN"
             elif binding == "$SEARCHING_BY_EMAIL":
+                # Is this a lookup by email address?
+                # Specifically, project leads/admins should be allowed to look
+                # up member info for members not in their project by email address,
+                # to support adding members by email address
                 if 'match' in options and 'MEMBER_EMAIL' in options['match']:
                     for subject in subjects:
                         bindings_by_subject[subject][binding] = "SEARCHING_BY_EMAIL"
