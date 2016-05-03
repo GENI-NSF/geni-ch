f71ed2669cd7af2fb6fcc5082bf0bca4bf4c489f
Tom Mitchell <tmitchell@bbn.com>
2016-05-03 17:55:54 -0400

    Cleanups
    Listen to pep8.py by doing some suggested clean ups. Add copyright
    header, and fix copyright dates for new file.

--- tools/ABACKeyId.py
+++ tools/ABACKeyId.py
@@ -1,4 +1,26 @@
-#!/usr/bin/python
+# ----------------------------------------------------------------------
+# Copyright (c) 2016 Raytheon BBN Technologies
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and/or hardware specification (the "Work") to
+# deal in the Work without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Work, and to permit persons to whom the Work
+# is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Work.
+#
+# THE WORK IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE WORK OR THE USE OR OTHER DEALINGS
+# IN THE WORK.
+# ----------------------------------------------------------------------
+
 import os
 import subprocess
 import sys
@@ -6,24 +28,25 @@ import tempfile
 import time
 from chapi_log import *
 
+
 # Maintain a global cache of mappings of raw certs and cert files to keyid
 class ABACKeyIdCache:
 
     def __init__(self):
         self.keyid_by_cert = {}
         self.keyid_by_cert_filename = {}
-        self.timestamp_by_cert = {}    
+        self.timestamp_by_cert = {}
         self.timestamp_by_cert_filename = {}
 
     # Whether to print chapi_error log messages on cache actions
     VERBOSE = False
 
     # Max number of entries in either cache
-    MAX_ENTRIES = 200  
+    MAX_ENTRIES = 200
 
-    # Max number of seconds since lookup we allow to remain in cache 
+    # Max number of seconds since lookup we allow to remain in cache
     # (if we've gone past MAX_ENTRIES)
-    CACHE_LIFETIME_SEC = 86400 
+    CACHE_LIFETIME_SEC = 86400
 
     def dump(self):
         if ABACKeyIdCache.VERBOSE:
@@ -35,8 +58,8 @@ class ABACKeyIdCache:
     # Prune entries in the given cache (cert or cert_filename)
     # If we have too many entries, delete all entries that haven't been looked up
     # in the last CACHE_LIFETIME_SEC seconds
-    def prune(self, certkey_keyid_mapping, certkey_timestamp_mapping): 
-#        self.dump()
+    def prune(self, certkey_keyid_mapping, certkey_timestamp_mapping):
+        # self.dump()
         certkey_to_delete = []
         time_limit = time.time() - ABACKeyIdCache.CACHE_LIFETIME_SEC
         if len(certkey_keyid_mapping) > ABACKeyIdCache.MAX_ENTRIES:
@@ -45,11 +68,11 @@ class ABACKeyIdCache:
                     certkey_to_delete.append(certkey)
         for certkey in certkey_to_delete:
             if ABACKeyIdCache.VERBOSE:
-                chapi_error('KEY_ID', "Removing keyid from cache: %s" % \
-                                certkey_keyid_mapping[certkey])
+                chapi_error('KEY_ID', "Removing keyid from cache: %s" %
+                            certkey_keyid_mapping[certkey])
                 del certkey_keyid_mapping[certkey]
                 del certkey_timestamp_mapping[certkey]
-        
+
 #        self.dump()
 
     # Lookup keyid from cert from cache. Return keyid or None if not cached
@@ -71,8 +94,8 @@ class ABACKeyIdCache:
             keyid = self.keyid_by_cert_filename[cert_filename]
             self.timestamp_by_cert_filename[cert_filename] = time.time()
             if ABACKeyIdCache.VERBOSE:
-                chapi_error("KEY_ID", "Returning cached keyid %s for file %s" % \
-                                (keyid, cert_filename))
+                chapi_error("KEY_ID", "Returning cached keyid %s for file %s" %
+                            (keyid, cert_filename))
         return keyid
 
     # Register relationship between raw cert and keyid
@@ -85,12 +108,13 @@ class ABACKeyIdCache:
     # Register relationship between cert_filename and keyid
     def register_cert_filename(self, cert_filename, keyid):
         if ABACKeyIdCache.VERBOSE:
-            chapi_error("KEY_ID", "Registering keyid %s for certfile %s" % \
-                            (keyid, cert_filename))
+            chapi_error("KEY_ID", "Registering keyid %s for certfile %s" %
+                        (keyid, cert_filename))
         self.keyid_by_cert_filename[cert_filename] = keyid
         self.timestamp_by_cert_filename[cert_filename] = time.time()
 
-ABAC_KEYID_CACHE = ABACKeyIdCache() # Singleton instance
+ABAC_KEYID_CACHE = ABACKeyIdCache()  # Singleton instance
+
 
 # Compute the ABAC keyid of a raw cert
 # Check if cert is in cert, if so return kdyid
@@ -102,6 +126,7 @@ def compute_keyid_from_cert(cert, cert_filename):
         keyid = compute_keyid_from_cert_file(cert_filename)
         ABAC_KEYID_CACHE.register_cert(cert, keyid)
     return keyid
+
 
 # Compute the ABAC keyid of a cert file
 # Compute the sha1 of the DER of bits of the public key in the cert
@@ -115,7 +140,6 @@ def compute_keyid_from_cert_file(cert_filename):
     keyid = ABAC_KEYID_CACHE.lookup_for_cert_filename(cert_filename)
     if keyid:
         return keyid
-
 
     # Get the public key from the cert
     # Run openssl x509 -pubkey -noout -in cert_filename -out pubkey.out
@@ -134,22 +158,22 @@ def compute_keyid_from_cert_file(cert_filename):
     lines = output.split("\n")
     bit_string_start = None
     for line in lines:
-#        print " LINE = " + line
+        # print " LINE = " + line
         if "BIT STRING" in line:
             parts = line.strip().split(':')
             bit_string_start = parts[0]
 #            print "BIT_STRING_START = " + bit_string_start
             break
 
-    if bit_string_start == None:
+    if bit_string_start is None:
         print "Can't find start of bit string in key asn1parse output"
         sys.exit(0)
 
     # Parse the keyfile starting at the bit string start and save as der file
     derfile = tempfile.NamedTemporaryFile(delete=False)
 #    print "DERFILE = " + derfile.name
-    args = ['openssl',  'asn1parse', '-in', keyfile.name, '-strparse', bit_string_start, 
-            '-out', derfile.name]
+    args = ['openssl', 'asn1parse', '-in', keyfile.name, '-strparse',
+            bit_string_start, '-out', derfile.name]
     output = subprocess.check_output(args)
 
     # Compute the SHA1 hash of the DER file of the public key bits
@@ -158,7 +182,7 @@ def compute_keyid_from_cert_file(cert_filename):
     parts = output.strip().split("= ")
     keyid = parts[1]
     # Replace ':' separators if they are in the returned SHA
-    keyid = keyid.replace(':', '') 
+    keyid = keyid.replace(':', '')
 
     ABAC_KEYID_CACHE.register_cert_filename(cert_filename, keyid)
 
@@ -173,4 +197,3 @@ if __name__ == "__main__":
         sys.exit(0)
 #    keyid = compute_keyid(sys.argv[1])
     print "KEYID = " + keyid
-
