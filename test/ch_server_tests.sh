d79cde8a7d6fe123bd19c053bfe5a3df2753152b
Marshall Brinn <mbrinn@bbn.com>
2016-06-08 14:56:16 -0400

    Investigate why connection refused

--- test/ch_server_tests.sh
+++ test/ch_server_tests.sh
@@ -15,40 +15,51 @@ netstat -an | grep 9999
 ps auxw | grep test
 tail /tmp/test_server.log
 
-export PYTHONPATH=$HOME/geni-tools/src:$CHAPIDIR
+export PYTHONPATH=$PYTHONPATH:$CHAPIDIR
 echo "PYTHONPATH = $PYTHONPATH"
 
 #    Make a first user, priv
 PRIV_EPPN=priv@geni.net
 python $CHAPIDIR/tools/client.py --method create_member --url https://localhost:9999/MA --key /usr/share/geni-ch/ma/ma-key.pem --cert /usr/share/geni-ch/ma/ma-cert.pem --string_arg=$PRIV_EPPN --raw_output > /tmp/priv-raw.json
+cat /tmp/priv-raw.json
 PRIV_URN=`python $CHAPIDIR/tools/json_extractor.py value,name=urn,value /tmp/priv-raw.json`
 
 python $CHAPIDIR/tools/client.py --method create_certificate --url https://localhost:9999/MA --key /usr/share/geni-ch/ma/ma-key.pem  --cert /usr/share/geni-ch/ma/ma-cert.pem --urn $PRIV_URN
 
 printf "{\"match\" : {\"_GENI_MEMBER_EPPN\" : \"%s\"}}\n" $PRIV_EPPN > /tmp/lookup_priv.json
+cat /tmp/lookup_priv.json
 
 # Grab certs and keys
 python $CHAPIDIR/tools/client.py --method lookup_login_info --url https://localhost:9999/MA --key /usr/share/geni-ch/ma/ma-key.python $CHAPIDIR/tools/client.py --method lookup_login_info --url https://localhost:9999/MA --key /usr/share/geni-ch/ma/ma-key.pem  --cert /usr/share/geni-ch/ma/ma-cert.pem --options_file /tmp/lookup_priv.json --raw_output > /tmp/priv.json
+cat /tmp/priv.json
 
 python $CHAPIDIR/tools/json_extractor.py   value,urn:$PRIV_URN,_GENI_MEMBER_SSL_PRIVATE_KEY  /tmp/priv.json > /tmp/priv-key.pem
+cat /tmp/priv-key.pem
 
 python $CHAPIDIR/tools/json_extractor.py   value,urn:$PRIV_URN,_GENI_MEMBER_SSL_CERTIFICATE  /tmp/priv.json > /tmp/priv-cert.pem
+cat /tmp/priv-cert.pem
 
 # Make a second user, unpriv
 UNPRIV_EPPN=unpriv3@geni.net
 python $CHAPIDIR/tools/client.py --method create_member --url https://localhost:9999/MA --key /usr/share/geni-ch/ma/ma-key.pem --cert /usr/share/geni-ch/ma/ma-cert.pem --string_arg=$UNPRIV_EPPN --raw_output > /tmp/unpriv-raw.json
+cat /tmp/unpriv-raw.json
 UNPRIV_URN=`python $CHAPIDIR/tools/json_extractor.py value,name=urn,value /tmp/unpriv-raw.json`
 
 python $CHAPIDIR/tools/client.py --method create_certificate --url https://localhost:9999/MA --key /usr/share/geni-ch/ma/ma-key.pem  --cert /usr/share/geni-ch/ma/ma-cert.pem --urn $UNPRIV_URN
 
 printf "{\"match\" : {\"_GENI_MEMBER_EPPN\" : \"%s\"}}\n" $UNPRIV_EPPN > /tmp/lookup_unpriv.json
+cat /tmp/lookup_unpriv.json
 
 # Grab certs and keys
 python $CHAPIDIR/tools/client.py --method lookup_login_info --url https://localhost:9999/MA --key /usr/share/geni-ch/ma/ma-key.python $CHAPIDIR/tools/client.py --method lookup_login_info --url https://localhost:9999/MA --key /usr/share/geni-ch/ma/ma-key.pem  --cert /usr/share/geni-ch/ma/ma-cert.pem --options_file /tmp/lookup_unpriv.json --raw_output > /tmp/unpriv.json
+cat /tmp/unpriv.json
 
 python $CHAPIDIR/tools/json_extractor.py   value,$UNPRIV_URN,_GENI_MEMBER_SSL_PRIVATE_KEY  /tmp/unpriv.json > /tmp/unpriv-key.pem
+cat /tmp/unpriv-key.pem
 
 python $CHAPIDIR/tools/json_extractor.py   value,$UNPRIV_URN,_GENI_MEMBER_SSL_CERTIFICATE  /tmp/unpriv.json > /tmp/unpriv-cert.pem
+cat /tmp/unpriv-cert.pem
+
 
 
 
@@ -56,6 +67,8 @@ python $CHAPIDIR/tools/json_extractor.py   value,$UNPRIV_URN,_GENI_MEMBER_SSL_CE
 
 
 #  At the end, kill the server
-killall test_server.py
+jobs
+ps auxw | grep python
+
 
 
