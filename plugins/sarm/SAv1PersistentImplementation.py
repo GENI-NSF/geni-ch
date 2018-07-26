53a1191ee3ee8e0f59f906f6f53511fcda3a8d95
Hussamuddin Nasir <nasir@netlab.uky.edu>
2018-07-26 12:42:08 -0400

    This patch now sets the slice certificate expiry time to be whatever the slice's actual expiry time is.

--- plugins/sarm/SAv1PersistentImplementation.py
+++ plugins/sarm/SAv1PersistentImplementation.py
@@ -665,8 +665,9 @@ class SAv1PersistentImplementation(SAv1DelegateBase):
         # FIXME: Why is the cert lifeDays 365 days more than the diff between slice expiration and creation?
         cert, k = cert_util.create_cert(slice.slice_urn, \
             issuer_key = self.key, issuer_cert = self.cert, \
-            lifeDays = (slice.expiration - slice.creation).days + \
-                       SA.SLICE_CERT_LIFETIME, \
+            lifeDays = (slice.expiration - dt.datetime.utcnow()).days,\
+  #          lifeDays = (slice.expiration - slice.creation).days + \
+  #                     SA.SLICE_CERT_LIFETIME, \
             email = slice.slice_email, uuidarg=slice.slice_id)
         slice.certificate = cert.save_to_string()
 
@@ -790,7 +791,8 @@ class SAv1PersistentImplementation(SAv1DelegateBase):
                 if (cert_exp < new_exp):
                     cert, k = cert_util.create_cert(slice_urn, \
                                                         issuer_key = self.key, issuer_cert = self.cert, \
-                                                        lifeDays = (new_exp - slice_info.creation).days + SA.SLICE_CERT_LIFETIME, \
+                                                        #lifeDays = (new_exp - slice_info.creation).days + SA.SLICE_CERT_LIFETIME, \
+                                                        lifeDays = (new_exp - dt.datetime.utcnow()).days , \
                                                         email = slice_info.slice_email, uuidarg=slice_info.slice_id)
                     # FIXME: Ticket #149: Save the slice key and
                     # re-use it when re-generating the slice certifate
